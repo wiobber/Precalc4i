2e3e84555b5526d2e3bd0f75950349dbda3b435e
Wim Obbels <wim@obbels.be>
2025-09-25 09:16:12 +0200

    remove obsolete scripts

--- dbg.py
+++
@@ -1,8 +0,0 @@
-# debug_openai.py
-import openai, inspect, sys, pathlib
-
-print(">>> openai.__file__ :", openai.__file__)          # full path to the module
-print(">>> openai.__package__ :", openai.__package__)    # should be 'openai'
-print(">>> sys.path[0] :", sys.path[0])                  # current working dir
-print(">>> cwd :", pathlib.Path.cwd())
-print(">>> has Batch? :", hasattr(openai, "Batch"))
